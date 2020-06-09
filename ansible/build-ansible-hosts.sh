9a15ff413c35e11171e7dd6bc59ff02368819b29
Brian Pitta <Brian.Pitta@illumio.com>
2020-06-09 12:49:28 -0400

    removed hard-coded json

--- ansible/build-ansible-hosts.sh
+++ ansible/build-ansible-hosts.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+# The first argument is the variables file name - no directory
+
 # Remove existing hosts file
 rm -f ../ansible/hosts > /dev/null 2>&1
 
@@ -9,9 +13,9 @@ data_nodes=()
 windows_wklds=()
 
 # Check if we are building an SNC and populate
-if [ $(cat variable-files/personal-lab.json | jq -r .pce.cluster_type) == "snc" ]; then
+if [ $(cat ${1} | jq -r .pce.cluster_type) == "snc" ]; then
     echo "[pce]" >> ../ansible/hosts
-    echo snc-$(cat variable-files/personal-lab.json | jq -r .pce.org_name).poc.segmentationpov.com node_type=snc0 >> ../ansible/hosts
+    echo snc-$(cat ${1} | jq -r .pce.org_name).poc.segmentationpov.com node_type=snc0 >> ../ansible/hosts
     echo "" >> ../ansible/hosts
     echo "[pce:vars]" >> ../ansible/hosts
     echo "ansible_user=centos" >> ../ansible/hosts
@@ -20,12 +24,12 @@ if [ $(cat variable-files/personal-lab.json | jq -r .pce.cluster_type) == "snc" 
 fi
 
 # Check if we are building an MNC and populate
-if [ $(cat variable-files/personal-lab.json | jq -r .pce.cluster_type) == "mnc" ]; then
+if [ $(cat ${1} | jq -r .pce.cluster_type) == "mnc" ]; then
     echo "[pce]" >> ../ansible/hosts
-    echo core0-$(cat variable-files/personal-lab.json | jq -r .pce.org_name).poc.segmentationpov.com node_type=core0 >> ../ansible/hosts >> ../ansible/hosts
-    echo core1-$(cat variable-files/personal-lab.json | jq -r .pce.org_name).poc.segmentationpov.com node_type=core1 >> ../ansible/hosts >> ../ansible/hosts
-    echo data0-$(cat variable-files/personal-lab.json | jq -r .pce.org_name).poc.segmentationpov.com node_type=data0>> ../ansible/hosts
-    echo data1-$(cat variable-files/personal-lab.json | jq -r .pce.org_name).poc.segmentationpov.com node_type=data1>> ../ansible/hosts
+    echo core0-$(cat ${1} | jq -r .pce.org_name).poc.segmentationpov.com node_type=core0 >> ../ansible/hosts >> ../ansible/hosts
+    echo core1-$(cat ${1} | jq -r .pce.org_name).poc.segmentationpov.com node_type=core1 >> ../ansible/hosts >> ../ansible/hosts
+    echo data0-$(cat ${1} | jq -r .pce.org_name).poc.segmentationpov.com node_type=data0>> ../ansible/hosts
+    echo data1-$(cat ${1} | jq -r .pce.org_name).poc.segmentationpov.com node_type=data1>> ../ansible/hosts
     echo "" >> ../ansible/hosts
     echo "[pce:vars]" >> ../ansible/hosts
     echo "ansible_user=centos" >> ../ansible/hosts
@@ -39,14 +43,14 @@ while read -r line; do
     if [ "$line" != "" ]; then
         linux_wklds+=("$line")
     fi
-done <<< "$(cat variable-files/personal-lab.json | jq -r '.linux_wklds | keys[]')"
+done <<< "$(cat ${1} | jq -r '.linux_wklds | keys[]')"
 
 # Get Windows workloads
 while read -r line; do
   if [ "$line" != "" ]; then
     windows_wklds+=("$line")
   fi
-done <<< "$(cat variable-files/personal-lab.json | jq -r '.windows_wklds | keys[]')"
+done <<< "$(cat ${1} | jq -r '.windows_wklds | keys[]')"
 
 # Write the Linux hosts if it's not the ansible server
 if [ ${#linux_wklds[@]} -gt 0 ]; then
